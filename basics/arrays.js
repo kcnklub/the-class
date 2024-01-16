880caf078bb83c2cfb6a9d9749292f61c157a16b
Kyle Miller <kylemiller457@gmail.com>
2024-01-15 19:09:24 -0600

    checkpoint

--- basics/arrays.js
+++ basics/arrays.js
@@ -11,23 +11,61 @@ console.log(names);
 // accessing values by index
 console.log(names[1]);
 
+for (let index = 0; index < names.length; index++) {
+    console.log(`index ${names[index]}`);
+}
+
 // iterate over contents
-for (const name of names) {
+for (let index in names) {
+    console.log(index);
+    console.log(names[index]);
+}
+for (let name of names) {
     console.log(name);
+}
+
+function map(_) {
+
 }
 
 
 // functional helpers
+//
 // map
+// Allows us to transform each element in the array
 let upperNames = names.map(name => name.toUpperCase());
 console.log(upperNames);
 console.log(names);
 
 // filter
+// Allows us to filter out elements from the array
 let containsT = names.filter(name => name.includes('t'));
 console.log(containsT);
 console.log(names);
 
 // reduce
-let nameString = names.reduce((acc, name) => acc + name + ' ', 'Hello! ');
+// Allows us to reduce the array to a single value
+let nameString = names.reduce(
+    (acc, name) => acc + name + ' ',
+    'Hello! '
+);
 console.log(nameString);
+
+const values = [1, 2, 3, 4, 5];
+const sum = values.reduce((acc, currentValue) => {
+    console.log(`acc ${acc}, currentValue ${currentValue}`);
+    return acc + currentValue
+}, 0);
+console.log(`sum ${sum}`)
+
+// Parsing strings
+let data = 'Kyle, kyle, Garrett, Steven';
+let namesArray = data.split(', ');
+console.log(namesArray);
+
+// Joining strings
+let namesString = namesArray.join(', ');
+console.log(namesString);
+
+// Sorting
+console.log(namesArray.sort());
